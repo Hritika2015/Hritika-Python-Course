1f960528645956c3999efc63a087d9956e332ba1
Hritika2015 <swapna3.rao@gmail.com>
2025-03-26 17:43:40 +0000

    Data Types

---
+++ 2 Class.py
@@ -0,0 +1,17 @@
+name = "Riya"
+age = 16
+is_sudent =  True
+weight = 24.8
+print ("Name :",name)
+print("Data Type of Name is", type(name))
+print ("Age:",age)
+print("Data Type of Age is", type (age))
+print ("is_sudent:", is_sudent)
+print ("Weight :", (weight))
+print("/n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+wegiht = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
--- First Class.py
+++ First Class.py
@@ -1,1 +1,15 @@
-print("Hello Dear Master")
+import keyword
+print("Hello Dear Master\n")
+print (1) 
+print("Well well well im am AI and im smarter that everyone! muhahaha i beat this many people! its a millon",3)
+A= 3
+B= 10
+C= 15
+D= 20
+E=2
+F=1
+G=9
+H= "MAYBE you HAVE beaten me in Storys but still im BETTER PRETTIER AND SMARTER than you."
+print (H)
+Name = input ("enter your name: ")
+print (keyword.kwlist)
