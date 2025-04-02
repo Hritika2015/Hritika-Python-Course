ff54fa798b7f7db4472bd7d2e9a0706214db7bfa
Hritika2015 <swapna3.rao@gmail.com>
2025-04-02 17:53:05 +0100

    Conditional Statements

---
+++ 4 Class.py
@@ -0,0 +1,6 @@
+i = int(input("enter a number :"))
+if (i < 15):
+    print ("i is smaller than 15")
+    print ("im in else block")
+else:
+ print (" im not in if and not in else Block")
---
+++ Homework1.py
@@ -0,0 +1,6 @@
+num = 3
+if num > 0:
+ print (num,"that is a postive number.")
+num = -1
+if num > 0:
+ print (num,"THATS A POSTIVE NUMBER GURL")
