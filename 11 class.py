2a306f34237fb2f66e8bedb72006c0dbb0a7dd24
Hritika2015 <swapna3.rao@gmail.com>
2025-04-30 17:52:46 +0100

    Nested Loops

---
+++ 11 class.py
@@ -0,0 +1,3 @@
+i=0
+while i<=0:
+    print(" I...Iigeh... am STEVE! your worst FLINT AND STEAL! the CHICKEN JOCKEY! 'no, he isnt a king Hes a legend'- Tenchonoblade...THE NEATHER!!!La-la-lava! Ch-ch-ch-Chicken stevess lava chicken yeah as tasty as Heaven! ooo mama stia now your ringing the bell!! oohhh super spicy its a lava actackkkkkkk!!!")
---
+++ homeworkkk.py
@@ -0,0 +1,5 @@
+Sugar = 24
+Ring = 15
+Diamond = 21
+Candy = 31
+Sweet = 8
