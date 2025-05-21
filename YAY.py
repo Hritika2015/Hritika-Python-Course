92b20c93b0632b4c2ae77a7cc233356f5222a367
Hritika2015 <swapna3.rao@gmail.com>
2025-05-21 17:39:21 +0100

    Keywords

---
+++ YAY.py
@@ -0,0 +1,7 @@
+a = input ("Enter a Word (small letters even for name or e.t.c): ")
+for i in a:
+    if (i == 'a'):
+        print("A is found, so um- yay?!")
+    else:
+        print("A is not found, so yeah! ")
+#This folder of very super,big,really peak folder was supposed to be named, "17th class.py" Thanks for reading this, if you dont mind me yeah im gonna get really mellay angry at you beacause why not? hahaha im angry at you so yeah. bye i guess! :D no actually this im to lazy to remove >:D no this actuallly :0 no no this :) actually this: :( forgot that this >:(
