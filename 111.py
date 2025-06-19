9a1429278c8204ddc05c48302c8b8423724f6495
Hritika2015 <swapna3.rao@gmail.com>
2025-06-19 17:55:42 +0100

    yay

---
+++ 111.py
@@ -0,0 +1,8 @@
+set1 = {'Purple', 'Pink'}
+set2 = {'Purple', 'Candy Floss Pink'}
+print("Orignal Elements:")
+print(set1)
+print(set2)
+print("\nSymmetic different of sets")
+set1 = set1.symmetric_difference(set2)
+print(set2)
