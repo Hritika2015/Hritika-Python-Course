f2cb50c81091d1b6143740aa1c71f49338209f2f
Hritika2015 <swapna3.rao@gmail.com>
2025-07-10 17:43:56 +0100

    rizzy gyatt

---
+++ uuuuu.py
@@ -0,0 +1,13 @@
+class myClass:
+
+        __privateVar = 27;
+        
+        def __privMeth(self):
+                print(" IM FINALLY FREE FROM WLRD WAR 500000000")
+
+                def hello(self):
+                        print(" EW. ", myClass.__privateVar)
+
+foo = myClass()
+foo.hello()
+foo.__privMeth
