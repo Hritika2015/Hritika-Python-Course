455c505bfccf8ce3cf1570e94c54451ede5033ed
Hritika2015 <swapna3.rao@gmail.com>
2025-07-24 16:42:25 +0100

    angry birds

---
+++ Homeworkyas.py
@@ -0,0 +1,20 @@
+import math
+
+class Circle:
+        def __init (self,radius):
+            self.radius = radius
+
+        def area(self):
+            return math.pi * self.radius ** 2
+        
+        def perimeter(self):
+            return 2 * math.pi * self.radius
+
+radius_input = float(input("Enter the radius of the circle:")) 
+print(" I am angry joking!! ")
+my_circle = Circle(radius_input)
+
+print("Area of circle: ", my_circle.area())
+print("Perimeter of the circle: ", my_circle.perimeter)
+        
+        
