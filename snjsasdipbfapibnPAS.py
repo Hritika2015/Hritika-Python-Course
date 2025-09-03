3ee0bc5a4bec757817ea6d8c1b78cc72329e2842
Hritika2015 <swapna3.rao@gmail.com>
2025-09-03 17:48:51 +0100

    ddfdf

---
+++ snjsasdipbfapibnPAS.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+window = Tk()
+window.title("Congial Text Editor")
+window.geometry("600x500")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+def open_file():
+    """Open a file for editing"""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete(1.0, e=END)
+    with open(filepath,"r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(END,text)
+        input_file.close()
+    window.tittle(f"congials Text Editor - {filepath}")
+def save_file():
+    filepath = asksaveasfilename(defaltextension="txt",
+                                 filetypes=[("Text files", "*.txt"), ("All Files", "*.*")],
+)
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, END)
+        output_file.write(text)
+        window.title(f"conglials Text Editor - {filepath}")
+txt_edit = Text(window)
+fr_buttons = Frame(window, relief=RAISED, bd=2 ) 
+btn_open = Button(fr_buttons,text="Open", commands=open_file)
+btn_save = Button(fr_buttons,text="Save As....", command=save_file)
+print("please school start summer break agian")
+
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_open.grid(row=0, column=0, sticky="ew", padx=5)
+fr_buttons.grid(row=0, column=0, sticky="ns")
+fr_buttons.grid(row=0, column=1, sticky="nsew")
+window.mainloop()
